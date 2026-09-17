7f108151b353d4d83b85b093b7deba5e29b00f8d
elizabethann5 <anntheresp44e@gmail.com>
2026-09-17 11:44:58 +0530

    Rename model file to student_pass_fail_model_modified.pkl

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import joblib
 from pathlib import Path
 
-model_path = Path(__file__).parent / "student_pass_fail_model.pkl"
+model_path = Path(__file__).parent / "student_pass_fail_model_modified.pkl"
 model = joblib.load(model_path)
 
 st.title("Student Pass Predictor")
